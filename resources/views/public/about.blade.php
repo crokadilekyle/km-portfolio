d086a6792f178956d9678d2ff5718b93bcda7bb8
crokadilekyle <kyle@twinwebdev.com>
2020-12-26 19:03:53 -0600

    updated about page

--- resources/views/public/about.blade.php
+++ resources/views/public/about.blade.php
@@ -9,6 +9,7 @@
             <p>I first started coding websites back in the days of "clears" and "floats". Thankfully, the web has come a long way since then.</p>
             <p>Fast forward to {{ now()->year }} and I find myself much more interested in the data that makes modern websites work. It is amazing to me how much a website can do today. It's also amazing to me how many different tools we have at our disposal for creating these modern websites.</p>
             <p>So, which tools are the best? I believe there is no "one size fits all" approach to building websites and web apps. The requriements of the project determines which tools are best suited for the job. Sometimes a client side rendered SPA, using something like React or Vue, is the way to go. Other times a server rendered, multi-page application using something like Laravel or Django is a better approach. In other cases, a simple WordPress site is more than capable of getting the job done. I think one of a web developer's responsibilies is to be familiar with all of the different approaches and technologies available to them so they can choose the best stack to use when building a project. This is why I am committed to learning as many different technologies as I can.</p>
+            <h3>Technologies</h3>
             <p>Here are the technologies that I currently use or am familiar with:</p>
             <div class="tags-list">
                 <ul class="tags">
@@ -46,8 +47,10 @@
                 <li>Python</li>
                 <li>SQL</li>
             </ul>
-            <p>Another responsibility of web developers, in my opinion, is to know when not to overcomplicate things. Just because I know how to build a UI with React, it doesn't necessarily mean I should use it, along with it's 1500 plus dependencies, to build a brochure website with a contact form. Not that I'm saying dependencies are necessarily a bad thing - there is no sense in me "reinventing the wheel" either. If I need a bit of functionality that has been done before, and done well, there is no reason why I shouldn't include it into the project that I'm building if I can. The key is to find a happy balance and make sure that the end product works well and is maintainable.</p>
-            <p>This is just a bit about my philosophy on web development. If you want to learn more, hire me, see my resume, or just talk shop, please send me an email:
+            <h3>My Development Process</h3>
+            <p>When I take on a new project the first thing I do is determine what the client's needs are and what technology stack will best fit those needs. If necessary, I create wireframes and/or a database schema next. For almost every project I also create a GitHub Repository for version control and a Trello Board to help me prioritize my tasks. If the client is familiar with Trello I will give them access to the board. This way they can leave comments, report bugs, etc in Trello.</p>
+            <p>When I develop the website or app I usually build the basic skeleton of the project in the main GIT branch first. After that I create seperate branches for each different part the development process. I also create seperate branches when I am doing bug fixes or experimenting with different ideas. Often times I will also use GIT for deployment.</p>
+            <p>This is a basic summary of my development process and ofcourse it varies from project to project. If you want to learn more, hire me, see my resume, or just talk shop, please send me an email:
             </p>
             <ul class="tags email">
                 <li class="copy-li" data-clipboard-text="kyle@kylemerl.com">kyle@kylemerl.com<button class="btn-sm secondary copy-btn" data-clipboard-text="kyle@kylemerl.com"><i class="fa fa-copy"></i></button></li><span class="copied-success">Copied!</span>
